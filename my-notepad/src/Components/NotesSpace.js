c48bf4c505f4e76c767842d45e000f789b5337c6
Nicole-Dhm <Nicole.Dahmen@edu.ruhr-uni-bochum.de>
2025-09-16 16:08:04 +0200

    feat: basic Note Section plus Drawer

---
+++ my-notepad/src/Components/NotesSpace.js
@@ -0,0 +1,22 @@
+import { Box, List, ListItem } from '@mui/material';
+
+export default function NoteSpace() {
+    return (
+        <Box
+            component={'main'}
+            sx={{
+                flexGrow: 1,
+                border: '2px solid black',
+                borderRadius: 5,
+                p: 2,
+                m: 5,
+                mr: 50,
+                bgcolor: 'background.paper',
+            }}>
+            <h3>Your Notes</h3>
+            <List>
+                <ListItem> I'm a Note </ListItem> 
+            </List>
+        </Box>
+    );
+}
