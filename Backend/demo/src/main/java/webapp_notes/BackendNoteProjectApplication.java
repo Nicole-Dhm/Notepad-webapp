80559b2401f4a198fc7970898f37c86770560033
Nicole-Dhm <Nicole.Dahmen@edu.ruhr-uni-bochum.de>
2025-09-17 14:58:32 +0200

    chore: added PostgresSQL, added docker compose, added spring boot

---
+++ Backend/demo/src/main/java/webapp_notes/BackendNoteProjectApplication.java
@@ -0,0 +1,13 @@
+package webapp_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNoteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNoteProjectApplication.class, args);
+	}
+
+}
