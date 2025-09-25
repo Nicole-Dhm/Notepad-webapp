6d86576028e3c91569abc9804c66a932f8d498ea
Nicole-Dhm <Nicole.Dahmen@edu.ruhr-uni-bochum.de>
2025-09-25 18:35:15 +0200

    feat: added date and time

--- my-notepad/src/Components/Header.js
+++ my-notepad/src/Components/Header.js
@@ -1,10 +1,50 @@
-import { Box } from "@mui/material";
-import { Today } from "@mui/icons-material";
+import { Box, Stack } from "@mui/material";
+import { Today, QueryBuilder} from "@mui/icons-material";
 import { useTheme } from "@emotion/react";
 import Settings from "./SettingsInterface";
+import { useState, useEffect} from "react";
 
+//header function
 export default function Header() {
     const theme = useTheme();
+    // time and date states, pull from local
+    const [ time, setTime ] = useState('');
+    const [ date, setDate ] = useState('');
+
+    //load time and date on load in
+    useEffect(()=>{
+        function updateTime() {
+            // custom display,no american format
+            setTime(new Date().toLocaleTimeString([],{
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                hour12: false,
+            }
+            ));
+        };
+
+        function updateDate() {
+            setDate( new Date().toLocaleDateString('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+            }));
+        }
+
+        //immediate display
+        updateTime();
+        updateDate();
+
+        //timed upates 
+        const timeInterval = setInterval(updateTime, 1000);
+        const dateInterval = setInterval(updateDate, 60000);
+        return () => {
+            clearInterval(timeInterval);
+            clearInterval(dateInterval);
+        };
+    }, []);
+    
     return (
             <Box sx= {{ 
                 height: 60, 
@@ -18,8 +58,13 @@ export default function Header() {
                 boxShadow: '4px 4px 4px rgba(4, 30, 34, 0.3)',
                 p: 2
                 }}>
-                    <Box sx={{ width: 40 }}>
-                        <Today /> 
+                    <Box sx={{ width: 200 }}>
+                        <Stack direction={ 'row '} gap={1}>
+                            <Today />
+                            <span>{date}</span>
+                            <QueryBuilder />
+                            <span>{time}</span>
+                        </Stack>
                     </Box>
                     <h1 style={{ margin: 0 }}>Melonotes</h1>
                     <Settings />
