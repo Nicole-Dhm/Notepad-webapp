fe703800b1b0850bc7194e56881545d4796c8188
Nicole-Dhm <Nicole.Dahmen@edu.ruhr-uni-bochum.de>
2025-09-29 00:20:28 +0200

    feat: added delete option, REMINDER: I have persistent volume data so if i delete the notes they're gone gone unless i copy the init sql file inot docker container again

--- my-notepad/src/Components/Notes.js
+++ my-notepad/src/Components/Notes.js
@@ -1,23 +1,39 @@
-import { ListItem, useTheme, Stack, Box } from '@mui/material';
+import { ListItem, useTheme, Stack, Box, IconButton, Tooltip } from '@mui/material';
 import { useState, useEffect } from 'react';
+import { alpha } from '@mui/material';
+import { RemoveCircle } from '@mui/icons-material';
 
 export default function Notes(){
     const [notes, setNotes] = useState([]);
     const theme = useTheme();
-    useEffect(() => {
-        const currentNotes = async() => {
-            try {
-                const res = await fetch('http://localhost:8080/api/notes');
-                if(!res.ok) throw new Error ('featch: this broke');
-                const data = await res.json();
-                setNotes(data);
-            } catch(e) {
-                console.error(e);
-                }
-        };
+    
+    //get notes for display and iterating
+    const currentNotes = async() => {
+        try {
+            const res = await fetch('http://localhost:8080/api/notes');
+            if(!res.ok) throw new Error ('featch: this broke');
+            const data = await res.json();
+            setNotes(data);
+        } catch(e) {
+            console.error(e);
+            }
+    };
+    useEffect(()=> {
         currentNotes();
-    }, []);
+    }) 
 
+    //Deleting a note
+    const handleDelete = async(id) => {
+        try {
+            const res = await fetch(`http://localhost:8080/api/notes/${id}`, {
+                method: 'DELETE'
+            });
+            if (!res.ok) throw new Error ('Delete fetch failed')
+            currentNotes();
+        } catch(e) {
+            console.error(e)
+        }
+    }
     return(
         <>
         {/*Generate the List Items I pop back in in the NotesSpace */}
@@ -26,27 +42,29 @@ export default function Notes(){
             key= {note.id}
             sx={{
                 border: '2px solid',
-                borderColor: theme.palette.primary.main,
-                backgroundColor: theme.palette.background.paper,
+                borderColor: theme.palette.primary.dark,
+                backgroundColor: alpha(theme.palette.primary.main, 0.3),
                 borderRadius: 2,
                 height: 100,
                 mb: 2,
+                pt: 2,
                 display: 'flex',
                 justifyContent: 'space-between',
-                alignItems: 'center',
-                px: 2
+                alignItems: 'flex-start',
+                px: 2,
+                position: 'relative'
             }}>
-            <Stack direction={'column'} gap={2} sx={{ flexGrow: 1}}>
+            <Stack direction={'column'} gap={1} sx={{ flexGrow: 1}}>
                 <strong>{note.title}</strong>
                 {note.content}
             </Stack>
             <Box sx={{
                 fontSize: 12,
-                alignSelf: 'flex-start'
+                alignSelf: 'flex-end'
             }}>
                 {/*Custom Date Time format, Timestamp will become dynamic later */}
                 <strong>
-                {new Date(note.createdAt).toLocaleString('en-GB',{
+                Date: {new Date(note.createdAt).toLocaleString('en-GB',{
                     hour: '2-digit',
                     minute: '2-digit',
                     second: '2-digit',
@@ -57,6 +75,18 @@ export default function Notes(){
                 })}
             </strong>
             </Box>
+            <Tooltip title= 'Delete Note'>
+                <IconButton
+                    onClick={()=> handleDelete(note.id)}>
+                    <RemoveCircle
+                        sx={{
+                            color: theme.palette.primary.dark,
+                            position: 'absolute',
+                            top: 2,
+                            right: 2,
+                        }}/>
+                </IconButton>
+            </Tooltip>
             </ListItem>
         ))}
         </>
