ea1e47a334911310bbd422812cbafb74bbb7ef53
Nicole-Dhm <Nicole.Dahmen@edu.ruhr-uni-bochum.de>
2025-09-29 13:47:15 +0200

    chore: changed NoteSpace container

--- my-notepad/src/Components/NotesSpace.js
+++ my-notepad/src/Components/NotesSpace.js
@@ -1,4 +1,5 @@
 import { Box, List, useTheme } from '@mui/material';
+import { alpha } from '@mui/material';
 import Notes from './Notes';
 
 export default function NoteSpace() {
@@ -11,13 +12,32 @@ export default function NoteSpace() {
                     border: '2px solid',
                     borderColor: theme.palette.primary.main,
                     borderRadius: 5,
-                    p: 2,
-                    m: 5,
+                    mt: 5,
+                    ml: 5,
                     mr: 5,
                     bgcolor: 'background.paper',
+                    overflow: 'hidden',
+                    display: 'flex',
+                    flexDirection: 'column'
                 }}>
-                <h3>Your Notes</h3>
-                <List>
+                <Box 
+                sx={{
+                    width: '100%',
+                    backgroundColor: alpha(theme.palette.primary.main, 0.9),
+                    borderTopLeftRadius: 5,
+                    height: 65,
+                    display: 'flex',
+                    alignItems: 'center',
+                    color: theme.palette.primary.light,
+                    fontSize: 25,
+                    px: 4,
+                    flexShrink: 0,
+                    borderBottom: '2px solid',
+                    borderBottomColor: theme.palette.primary.main
+                }}>
+                    <strong>Your Notes</strong>
+                </Box>
+                <List sx={{ p: 2}}>
                     <Notes />
                 </List>
             </Box>
